ee5a6e36314f89572fd8d755c6205d1d8b1d9deb
Ris Adams <chris@tuesdaybegins.com>
2019-04-23 13:28:18 -0400

    Fixing bugs,  hopefully runs faster

--- WinArto/Program.cs
+++ WinArto/Program.cs
@@ -15,74 +15,22 @@ namespace WinArto
     {
         private static ChromiumWebBrowser _browser;
 
-        public static Task LoadPageAsync(ChromiumWebBrowser browser, string address, int width)
+        public static Task LoadPageAsync(ChromiumWebBrowser browser, string address, string basePath, string path, BreakPoint bp, string suffix)
         {
             var tcs = new TaskCompletionSource<bool>();
 
-            void OnHandler(object sender, LoadingStateChangedEventArgs args)
+            void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
             {
                 //Wait for while page to finish loading not just the first frame
                 if (!args.IsLoading)
                 {
-                    browser.LoadingStateChanged -= OnHandler;
+                    browser.LoadingStateChanged -= OnLoadingStateChanged;
                     tcs.TrySetResult(true);
-                }
-            }
 
-            browser.LoadingStateChanged += OnHandler;
-
-            browser.Size = new Size(width, 2500);
-            browser.Load(address);
-            return tcs.Task;
-        }
-
-        private static async Task GatherSnapshots(ChromiumWebBrowser browser, TestSet testSet)
-        {
-            var local = new Uri(testSet.LocalDomain);
-            var remote = new Uri(testSet.RemoteDomain);
-            var localBasePath = $"{Settings.Default.OutputFolder}/local/{GetHostFileName(local)}";
-            var remoteBasePath = $"{Settings.Default.OutputFolder}/remote/{GetHostFileName(remote)}";
-
-            if (!Directory.Exists(localBasePath))
-            {
-                Directory.CreateDirectory(localBasePath);
-            }
-
-            if (!Directory.Exists(remoteBasePath))
-            {
-                Directory.CreateDirectory(remoteBasePath);
-            }
-
-            foreach (var path in testSet.Paths)
-            {
-                ConsoleUtils.WriteLineColor($"Loading {local}\\{path}", ConsoleColor.White);
-                foreach (var bp in testSet.BreakPoints)
-                {
-                    var ssPath = $"{localBasePath}\\{bp.Name}";
-                    if (!Directory.Exists(ssPath))
-                    {
-                        Directory.CreateDirectory(ssPath);
-                    }
-
-                    ConsoleUtils.WriteLineColor($"\tFetching at breakpoint {bp.Name}", ConsoleColor.Gray);
-                    await LoadPageAsync(browser, new Uri(local, path).ToString(), bp.Width);
-                }
-
-                ConsoleUtils.WriteLineColor($"Loading {remote}\\{path}", ConsoleColor.White);
-                foreach (var bp in testSet.BreakPoints)
-                {
-                    var ssPath = $"{remoteBasePath}\\{bp.Name}";
-                    if (!Directory.Exists(ssPath))
-                    {
-                        Directory.CreateDirectory(ssPath);
-                    }
-
-                    ConsoleUtils.WriteLineColor($"\tFetching at breakpoint {bp.Name}", ConsoleColor.Gray);
-                    await LoadPageAsync(browser, new Uri(remote, path).ToString(), bp.Width);
-                    await browser.ScreenshotAsync()
+                    browser.ScreenshotAsync()
                         .ContinueWith(task =>
                         {
-                            var screenshotPath = Path.Combine(ssPath, $"{path}.jpg");
+                            var screenshotPath = Path.Combine(basePath, $"{path}_{bp.Name}_{suffix}.jpg");
 
                             ConsoleUtils.WriteLineColor($"Screenshot ready. Saving to {screenshotPath}", ConsoleColor.Yellow);
 
@@ -97,15 +45,23 @@ namespace WinArto
                         });
                 }
             }
-        }
 
-        private static string GetHostFileName(Uri url) => url.Host.TrimStart('/', '\\');
+            browser.LoadingStateChanged += OnLoadingStateChanged;
+
+            if (!string.IsNullOrEmpty(address))
+            {
+                browser.Size = new Size(bp.Width, 2500);
+                browser.Load(address);
+            }
+
+            return tcs.Task;
+        }
 
         /// <summary>
         ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        private static async Task Main()
+        public static async Task Main()
         {
             CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
             var settings = new CefSettings
@@ -113,7 +69,7 @@ namespace WinArto
                 CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), Application.ProductName, "BrowserCache"),
                 UserAgent = $"{Application.ProductName}/{Application.ProductVersion},(LIKE: Chrome/71.0.2)",
                 WindowlessRenderingEnabled = true,
-                LogSeverity = LogSeverity.Error
+                LogSeverity = LogSeverity.Info
             };
 
             Cef.Initialize(settings, true, browserProcessHandler: null);
@@ -122,7 +78,7 @@ namespace WinArto
             ConsoleUtils.WriteLineColor($"\t{settings.UserAgent}", ConsoleColor.Gray);
             ConsoleUtils.WriteLineColor("Loading Configuration", ConsoleColor.White);
 
-            _browser = new ChromiumWebBrowser();
+            _browser = new ChromiumWebBrowser(string.Empty);
 
             _browser.BrowserInitialized += async (s, e) =>
             {
@@ -131,12 +87,42 @@ namespace WinArto
 
             await Task.Run(() =>
             {
+                //Hack to get VS to stop complaining, does nothing
             });
 
-            Console.ReadKey(); //wait for something;
-
+            Console.ReadKey();
             Cef.Shutdown();
         }
+
+        private static async Task GatherSnapshots(ChromiumWebBrowser browser, TestSet testSet)
+        {
+            var local = new Uri(testSet.LocalDomain);
+            var remote = new Uri(testSet.RemoteDomain);
+            var basePath = $"{Settings.Default.OutputFolder}\\{GetHostFileName(remote)}";
+
+            if (!Directory.Exists(Settings.Default.OutputFolder))
+            {
+                Directory.CreateDirectory(Settings.Default.OutputFolder);
+            }
+
+            if (!Directory.Exists(basePath))
+            {
+                Directory.CreateDirectory(basePath);
+            }
+
+            foreach (var path in testSet.Paths)
+            {
+                foreach (var bp in testSet.BreakPoints)
+                {
+                    ConsoleUtils.WriteLineColor($"\tFetching {path} at breakpoint {bp.Name}", ConsoleColor.Gray);
+                    await LoadPageAsync(browser, new Uri(local, path).ToString(), basePath, path, bp, "local");
+                    await LoadPageAsync(browser, new Uri(remote, path).ToString(), basePath, path, bp, "remote");
+                }
+            }
+            
+        }
+
+        private static string GetHostFileName(Uri url) => url.Host.TrimStart('/', '\\');
 
         private static async Task MainAsync()
         {
@@ -144,10 +130,13 @@ namespace WinArto
             {
                 var json = File.ReadAllText(Settings.Default.Configuration);
                 var sets = JsonConvert.DeserializeObject<IEnumerable<TestSet>>(json);
+
+                await LoadPageAsync(_browser, "127.0.0.1", "", "", new BreakPoint {Width = 0}, "");
                 foreach (var set in sets)
                 {
                     await GatherSnapshots(_browser, set);
                 }
+                ConsoleUtils.WriteLineCautionTape("COMPLETE - Press Any Key");
             }
             else
             {
